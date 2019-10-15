f6bb5ca8c7ae874a639161e17adc52fcd14f5c76
Alex Hopkins <hopkinsalex@knights.ucf.edu>
2019-10-14 20:38:41 -0400

    Third

---
+++ examples/Third.py
@@ -0,0 +1,20 @@
+"""
+This is the third example. This example is an introductory lesson into
+object-oriented programming (OOP). Object-oriented programming is a programming paradigm based on the concept of
+"objects", which can contain data, in the form of fields, and code, in the form of procedures.
+
+What that means is in code you would have an object, Plant, that contains information about the Plant, like height,
+color and you have functions that control those fields of data. Like grow() to increase height.
+
+The usefulness of OOP is to encapsulate your data so things you don't expect to modify it cannot modify the information.
+It is also useful to hide data.
+
+Created by alex
+"""
+
+
+# This is a class. A Class is how you create and define the object.
+class Plant:
+    """
+    Plant class
+    """
